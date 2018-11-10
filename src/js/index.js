45829d59b2c187a83e24a1a08fbd1c6d71227f79
Zeztron <pateltharsh@gmail.com>
2018-11-10 10:52:21 -0500

    Added api key

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+async function getResults(query) {
+    const key = "1a832fb5a8d7dec043f06e57acc5ca13";
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch(error) {
+        alert(error);
+    }
+}
+getResults("pasta");
+
+
+// first API key: 1a832fb5a8d7dec043f06e57acc5ca13
+// Second API key: 9062950ff6364dd28553e8900f0c2a31
+// Third API key: c23cf38b29d6b2503e6e1de60bd0044d
+// Fourth API key: b58927e43a9759e8364a7373dcec2a72
+// Search: https://www.food2fork.com/api/search
+// Recipe: https://www.food2fork.com/api/get
